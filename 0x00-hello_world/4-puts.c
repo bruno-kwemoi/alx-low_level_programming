58e9fe4343c946ea328c8c983bb7f63e4e606b37
bruno-kwemoi <bsaekwo@gmail.com>
2021-08-29 09:11:49 +0300

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,7 +9,7 @@ int main(void)
 /*
  * prints an output
  */
-puts("Programming is like building a multilingual puzzle");
+puts(""Programming is like building a multilingual puzzle");
 return (0);
 
 }
