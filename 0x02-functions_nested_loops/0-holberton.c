8eeaa3be3b28aebfe8bb8e3d7700865bf938d56a
bruno-kwemoi <bsaekwo@gmail.com>
2021-08-31 13:33:39 +0300

    _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *main-Entry point
+ *Description: prints Holberton
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+	char name[] = "Holberton\n";
+	int i = 0;
+	while (name[i] != '\0')
+	{
+		char c = name[i];
+
+		_putchar(c);
+		i++;
+	}
+	return (0);
+}
