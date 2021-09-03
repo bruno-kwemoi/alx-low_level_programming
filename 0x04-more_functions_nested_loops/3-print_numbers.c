4143106526302a182daf1ad257654d5f249e0d7d
bruno-kwemoi <bsaekwo@gmail.com>
2021-09-03 23:00:53 +0300

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_numbers - prints numbers
+ *
+ *Return: Always 0.
+ */
+void print_numbers(void)
+{
+for(int i = 0; i<=9; i++)
+{
+printf("%d",i);
+}
+printf("$\n");
+}
