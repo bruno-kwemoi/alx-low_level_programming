867f1d71da1845ccd33b2b510283d25eb8eef67a
bruno-kwemoi <bsaekwo@gmail.com>
2021-09-06 19:55:40 +0300

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,8 @@
 /**
  * swap_int - swaps numbers
- *@*a - pointer
- *@*b - pointer
- *@c - integer
+ *@a: pointer
+ *@b: pointer
+ *@c: integer
  * Return: Always 0.
  */
 void swap_int(int *a, int *b)
