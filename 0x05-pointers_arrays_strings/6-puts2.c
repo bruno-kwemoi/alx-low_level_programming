1ba47dad3a32dec36246e3e070d52be824d94a5e
bruno-kwemoi <bsaekwo@gmail.com>
2021-09-07 15:19:05 +0300

    prints even characters

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,13 +8,14 @@
 void puts2(char *str)
 {
 int i = 0;
-while (str[i])
+
+while (str[i] != '\0')
 {
-if (str[i]%2==0)
+if (i % 2 == 0)
 {
 _putchar(str[i]);
-_putchar('\n');
 }
 i++;
 }
+_putchar('\n');
 }
