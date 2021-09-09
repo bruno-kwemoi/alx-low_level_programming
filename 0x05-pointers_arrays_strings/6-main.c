bfa7d42cf359a00659f802c82792b7612c06ff95
bruno-kwemoi <bsaekwo@gmail.com>
2021-09-09 15:44:35 +0300

    update

---
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+void puts2(char *str);
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "Holberton";
+    puts2(str);
+    return (0);
+}
---
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+void puts_half(char *str);
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "lOREM IPSUM";
+    puts_half(str);
+    return (0);
+}
