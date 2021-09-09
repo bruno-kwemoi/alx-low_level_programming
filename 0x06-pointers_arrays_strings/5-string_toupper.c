444881ad9eecf29e3bb4c33b81e658f368e1ae7a
bruno-kwemoi <bsaekwo@gmail.com>
2021-09-09 22:32:28 +0300

    lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+/**
+ * *string_toupper - to uppercase
+ *@str: pointer to array
+ *Return: uppercase of lowercase
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
