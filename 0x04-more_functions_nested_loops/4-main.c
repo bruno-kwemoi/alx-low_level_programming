179509fe8f2fee0596d813c61a7ed6a4fee8edc4
bruno-kwemoi <bsaekwo@gmail.com>
2021-09-11 13:15:27 +0300

    print numbers except 4&2

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+void print_most_numbers(void);
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_most_numbers();
+    return (0);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_most_numbers - prints numbers 0-9
+ *
+ *Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if ((i != '2') && (i != '4'))
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
