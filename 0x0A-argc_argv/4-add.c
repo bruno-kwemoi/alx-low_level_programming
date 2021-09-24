45cfe068dd13097cae54e4d98ff520351bd16319
bruno-kwemoi <bsaekwo@gmail.com>
2021-09-24 07:35:18 +0300

    updates

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 /**
  *main -  add numbers
  *@argc: number of commands
  *@argv: commadline values
- *Return: 1 for an error
  *Return: 0 for success
  */
 
 int main(int argc, char *argv[])
 {
-	int sum, i, j;
+	int sum, i, j, length;
 	char *ptr;
 
 	if (argc == 1)
@@ -22,12 +22,15 @@ int main(int argc, char *argv[])
 	for (i = 1; i < argc; i++)
 	{
 		ptr = argv[i];
+		length = strlen(ptr);
 
-		for (j = 1; j < argc; j++)
+		for (j = 0; j < length; j++)
 		{
 			if (isdigit(*(ptr + j)) == 0)
+			{
 			printf("Error\n");
 			return (1);
+			}
 		}
 		sum += atoi(argv[i]);
 	}
