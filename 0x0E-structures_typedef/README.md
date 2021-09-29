aeaee9f520aba89e8cf33e4c6f9b5b70021efc04
bruno-kwemoi <bsaekwo@gmail.com>
2021-09-29 05:19:21 +0300

    first commit

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+#introduction
+a series of projects done while learning about structures in c
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog -as the name suggest
+ *@name: of the dog
+ *@age: of the dog
+ *@owner: of the dog
+ *Description: prety detailed above
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
