dc1eb32db3384537c4144d4ca5495a74e9225393
bruno-kwemoi <bsaekwo@gmail.com>
2021-10-03 09:33:55 +0300

    corrections

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,17 +15,21 @@ int main(void)
 		{
 			printf("FizzBuzz");
 		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz");
-		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz");
 		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
 		else
 		{
 		printf("%d", i);
+		}
+		if (i != 100)
+		{
+			putchar(' ');
 		}
 	}
 	printf("\n");
