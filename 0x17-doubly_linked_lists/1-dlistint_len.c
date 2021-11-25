fab8c39d15af9b9283256f1055367b04f1000915
bruno-kwemoi <bsaekwo@gmail.com>
2021-11-25 14:42:19 +0300

    first commit

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include <stddef.h>
+#include "lists.h"
+/**
+ * dlistint_len - finds number of elements
+ * @h: pointer to head
+ * Return: number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+  const struct dlistint_s *temp = h;
+  int i = 0;
+  while (temp != NULL)
+    {
+      i++;
+      temp = temp->next;
+    }
+  return(i);
+}
