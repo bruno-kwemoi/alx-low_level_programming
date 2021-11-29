25f4f357b068f22acabd158e0eee79e477fb88be
bruno-kwemoi <bsaekwo@gmail.com>
2021-11-29 11:47:34 +0300

    betty

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- *
- *
- *
+ * free_dlistint - free a doubly linked list
+ * @head: pointer to the first node of the list
+ * Return: nothing
  */
 void free_dlistint(dlistint_t *head)
 {
-  struct dlistint_s *temp = head;
-  struct dlistint_s *next;
+struct dlistint_s *temp = head;
+struct dlistint_s *next;
 
-  while (temp != NULL)
-    {
-      next = temp->next;
-      free(temp);
-      temp = next;
-    }
-  head = NULL;
+while (temp != NULL)
+{
+next = temp->next;
+free(temp);
+temp = next;
+}
+head = NULL;
 }
