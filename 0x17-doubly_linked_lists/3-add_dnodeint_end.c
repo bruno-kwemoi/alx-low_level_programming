eb1bc7edf05463c3fc20c8b7025dddf958fa040f
bruno-kwemoi <bsaekwo@gmail.com>
2021-11-29 11:30:51 +0300

    betty

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,24 +8,29 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-  struct dlistint_s *temp, *trav;
-  temp = (struct dlistint_s*)malloc(sizeof(struct dlistint_s));
-  temp->prev = NULL;
-  temp->next = NULL;
-  temp->n = n;
-  temp->next = NULL;
-  trav = *head;
+struct dlistint_s *temp, *trav;
+temp = (struct dlistint_s *)malloc(sizeof(struct dlistint_s));
 
-  if (*head == NULL)
-    {
-      *head = temp;
-    }
-  else
-    {
-      while (trav->next != NULL)
-	trav = trav->next;
-      temp->prev = trav;
-      trav->next = temp;
-    }
-  return(*head);
+if (temp == NULL)
+{
+return (NULL);
+}
+temp->prev = NULL;
+temp->next = NULL;
+temp->n = n;
+temp->next = NULL;
+trav = *head;
+
+if (*head == NULL)
+{
+*head = temp;
+}
+else
+{
+while (trav->next != NULL)
+trav = trav->next;
+temp->prev = trav;
+trav->next = temp;
+}
+return(*head);
 }
