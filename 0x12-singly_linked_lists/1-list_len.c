9dd132426e403bf2bc42605cbd12402e363c8344
bruno-kwemoi <bsaekwo@gmail.com>
2022-01-19 21:51:23 +0300

    betty

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,12 +2,12 @@
 /**
  * list_len - returns the number of elements in a linked list
  * @h : pointer to the first node of the linked list
- * Return : the number of elements in a linked list
+ * Return: the number of elements in a linked list
  */
 size_t list_len(const list_t *h)
 {
 
-	const list_t* temp = h;
+	const list_t *temp = h;
 	int count = 0;
 
 	while (temp != NULL)
